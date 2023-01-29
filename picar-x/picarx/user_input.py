c865a59e7a2814c1c38e2136b5aaa7ae04090cd7
lukestroh <66746219+lukestroh@users.noreply.github.com>
2023-01-29 15:54:59 -0800

    Update user_input.py

--- picar-x/picarx/user_input.py
+++ picar-x/picarx/user_input.py
@@ -6,7 +6,7 @@ Luke Strohbehn
 import k_turn, parallel_park
 
 
-COMMAND_DICT: dict = {"1": parallel_park.main(), "2": k_turn.main()}
+COMMAND_DICT: dict = {"1": "parallel park", "2": "K-turn"}
 
 
 def main():
@@ -14,9 +14,13 @@ def main():
     for key, value in COMMAND_DICT.items():
         print(key, value)
 
-    comm = input("Please enter the number of the command and hit enter:\n")
+    comm = int(input("Please enter the number of the command and hit enter:\n").strip())
 
-    COMMAND_DICT[comm]
+    
+    if comm == 1:
+        parallel_park.main()
+    elif comm == 2:
+       k_turn.main()
 
     return
 
